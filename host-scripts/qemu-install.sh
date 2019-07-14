1837330ad1557e300c4a8e371a2e38f26c6797b6
mkwon0 <mkwon@camelab.org>
2019-07-14 14:33:00 +0900

    update

---
+++ host-scripts/qemu-install.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+sudo yum install -y qemu-kvm qemu-img virt-manager libvirt libvirt-python libvirt-client virt-install virt-viewer xorg-x11-server-Xorg xorg-x11-xauth xorg-x11-apps
+export DISPLAY=localhost:10.0
--- qemu-scripts/init.sh
+++ qemu-scripts/init.sh
@@ -3,27 +3,28 @@ NUM_CPU_HERE=16
 DIR_WORK=/home/mkwon
 DIR=/home/mkwon/qemu-init
 
+sudo yum install -y git screen vim
+## Add simple configuration to vi configuration
+cat <<EOF >$HOME/.vimrc
+let g:go_version_warning = 0
+vnoremap <S-D> :norm i#<CR>
+vnoremap <S-U> :norm ^x<CR>
+EOF
+
 ## Init packages
 #sudo yum install screen -y
 #sudo yum groupinstall "Development Tools" -y
 #sudo yum install ncurses-devel bison bc flex eltutils-libelf-devel openssl-devel elfutils-libelf-devel -y
 #sudo yum install git -y
 
-## Add simple configuration to vi configuration
-#cat <<EOF >$HOME/.vimrc
-#let g:go_version_warning = 0
-#vnoremap <S-D> :norm i#<CR>
-#vnoremap <S-U> :norm ^x<CR>
-#EOF
-
 ## Add nameserver
 #sudo sed -i '1 i\nameserver 8.8.8.8' /etc/resolv.conf
 #sudo sed -i '1 i\nameserver 8.8.4.4' /etc/resolv.conf
 
 ## Clone linux-5.0.7-swap
-cd $DIR_WORK
-git clone git@github.com:mkwon0/linux-5.0.7-swap.git
-cp qemu-init/.config linux-5.0.7-swap/
+#cd $DIR_WORK
+#git clone git@github.com:mkwon0/linux-5.0.7-swap.git
+#cp qemu-init/.config linux-5.0.7-swap/
 #cd linux-5.0.7-swap
 #make -j${NUM_CPU_HERE} -s
 #make modules -j${NUM_CPU_HERE} -s
